71e8ec74f95198de29e479055346ffa53358395b
Rika4698 <rikasharmin181@gmail.com>
2025-05-25 12:29:00 +0600

    create main tables

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -2,3 +2,32 @@
 
 --1. Crating database
 CREATE DATABASE conservation_db;
+
+-- 2. Table Creation:
+--Rangers Table
+CREATE TABLE rangers(
+    ranger_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    region VARCHAR(100) NOT NULL
+);
+
+
+-- Species Table
+CREATE TABLE species (
+    species_id SERIAL PRIMARY KEY,
+    common_name VARCHAR(100) NOT NULL,
+    scientific_name VARCHAR(100) NOT NULL,
+    discovery_date DATE NOT NULL,
+    conservation_status VARCHAR(100) NOT NULL
+)
+
+
+-- Sightings Table
+CREATE TABLE sightings(
+    sighting_id SERIAL PRIMARY KEY,
+    species_id INT REFERENCES species(species_id),
+    ranger_id INT REFERENCES rangers(ranger_id),
+    location VARCHAR(100) NOT NULL,
+    sighting_time TIMESTAMP NOT NULL,
+    notes TEXT
+)
