55b94a96cec68ec78388ec26aa56f0fafad4427b
Rika4698 <rikasharmin181@gmail.com>
2025-05-25 14:50:07 +0600

    find Pass sightings

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -65,3 +65,8 @@ INSERT INTO rangers (name, region) VALUES ('Derek Fox', 'Coastal Plains');
 
 -- Query 2:  Count unique species ever sighted
 SELECT COUNT (DISTINCT species_id) AS unique_species_count FROM sightings;
+
+
+-- Query 3: Sightings where location includes 'Pass'
+SELECT * FROM sightings 
+   WHERE location ILIKE '%Pass%';
